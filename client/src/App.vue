5145ba4b1a25e1eca4c57177938d508f8e1619f7
iQuick <iquickpublic@gmail.com>
2022-07-12 20:51:43 +0200

    application cards

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,57 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import AppTile from './components/AppTile.vue'
+import whatsappLogo from './assets/whatsapplogo.png'
+import telegramLogo from './assets/telegramlogo.png'
+import discordLogo from './assets/discordlogo.svg'
+
+const appsInfo = ref([
+  {
+    name: 'Discord',
+    icon: discordLogo,
+    colors: ['#4a62fa', '#9b40fb']
+  },
+  {
+    name: 'Telegram',
+    icon: telegramLogo,
+    colors: ['#3c7dd7', '#01d1ff']
+  },
+  {
+    name: 'WhatsApp',
+    icon: whatsappLogo,
+    colors: ['#00e274', '#00733a']
+  }
+])
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="wrapper">
+    <div class="apps">
+      <AppTile v-for="app in appsInfo" :key="app.name"
+      :appName="app.name" :appIcon="app.icon" :colors="app.colors" />
+      </div>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap';
+
+body
+{
+  background: #303030;
+  color: #fff;
+  font-family: 'Lato', sans-serif;
+  font-weight: 500;
+  margin: 0;
+  padding: 0;
+}
+
+.apps
+{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 }
 </style>
---
+++ client/src/components/AppTile.vue
@@ -0,0 +1,91 @@
+<script setup>
+import {onMounted, ref} from 'vue'
+import info from '../assets/info.png'
+import poweroff from '../assets/poweroff.png'
+
+const props = defineProps({
+  appName: {
+    type: String,
+    default: 'App Name'
+  },
+  appIcon: {
+    type: String,
+    default: 'https://via.placeholder.com/150'
+  },
+   colors: {
+        type: Array,
+        default: ['#fff', '#fff']
+    }
+})
+
+</script>
+
+<template>
+<div class="app-container">
+<img :src=info class="info">
+<h1>{{appName}}</h1>
+<span class="spacer"></span>
+<img class="mainimage" :src=appIcon>
+<span class="spacer"></span>
+<img :src=poweroff id="togglestate" class="icon">
+</div>
+</template>
+
+<style scoped>
+h1, h2, h3, h4, h5, h6
+{
+    padding: 0;
+    margin: 0;
+}
+
+.app-container
+{
+    position: relative;
+    border: 1px solid #000;
+    border-radius: 10px;
+    padding: 10px;
+    width: 200px;
+    height: max-content;
+    background: linear-gradient(-45deg, v-bind('colors[0]'), v-bind('colors[1]'));
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+}
+
+.app-container h1
+{
+    filter: drop-shadow(0px 0px 2px #000);
+}
+
+.mainimage
+{
+    width: 100px;
+    height: 100px;
+    filter: drop-shadow(0px 0px 2px #000);
+}
+
+.spacer
+{
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+.icon
+{
+    width: 50px;
+    height: 50px;
+}
+
+.info
+{
+    width: 30px;
+    height: 30px;
+    background: #fff;
+    border-radius: 50%;
+    position: absolute;
+    top: -5px;
+    left: -5px;
+}
+</style>
